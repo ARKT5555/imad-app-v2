905ae4d7026a62bb9fa36fb4ea63936d67bfdd2d
ARKT5555 <arkt5555@yahoo.co.in>
2017-03-07 21:08:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+   user: 'arkt5555',
+   database: 'arkt5555',
+   host: 'db.imad.hasura-app.io',
+   port: '5432',
+   password: process.env.DB_PASSWORD,
+};
 var app = express();
 app.use(morgan('combined'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:page', function (req, res) {
+  pool.query('SELECT * FROM articles WHERE title=$1', [req.params.page
+                                                 ], function(err, result) {
+    if (err) {
+ 
+    }
+    else
+    {
+      res.send(JSON.);
+    }
+  }) 
+  res.sendFile(path.join(_dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
